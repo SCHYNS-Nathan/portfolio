8e249d35616e986ac3e7325d1e995a56e49385fd
nath3 <nathan.schyns@student.hepl.be>
2022-05-23 21:57:13 +0200

    Working on index

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,22 +1,68 @@
 <?php get_header(); ?>
 <main class="index">
+    <div class="index__landscape">
+        <div class="landscape__logo">
+            <svg width="336" height="336" viewBox="0 0 336 336" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="168" cy="168" r="168" fill="#282F39"/>
+                <circle cx="160" cy="160" r="160" fill="#CF2E46"/>
+                <path d="M126.688 230.859V170.63M199.896 89.1445V149.373M126.688 170.63H93.7446C93.7446 170.63 57.1406 170.63 57.1406 135.202C57.1406 99.7731 93.7446 99.7731 93.7446 99.7731M126.688 170.63V124.573V89.1445L199.896 230.859V195.43V149.373M199.896 149.373H226.251C226.251 149.373 262.855 149.373 262.855 184.802C262.855 220.23 226.251 220.23 226.251 220.23" stroke="#F7F5EC" stroke-width="45" stroke-miterlimit="1.41471" stroke-linejoin="bevel"/>
+            </svg>
+        </div>
+        <div class="landscape__background">
+
+        </div>
+        <div class="landscape__middleground">
+
+        </div>
+        <div class="landscape__forground">
+
+        </div>
+    </div>
     <section class="index__presentation">
         <h2 class="presentation__title">
             Bien le bonjour !
         </h2>
 
+
     </section>
-    <section class="index__project">
-        <h2 class="project__title">
+    <section class="index__projects">
+        <h2 class="projects__title">
             Jetez un œil à mes projets
         </h2>
 
+
+        <article class="projects__project">
+            <h3 class="project__title">
+
+            </h3>
+            <img class="project__image" src="" alt="">
+            <p class="project__description">
+
+            </p>
+            <a class="project__link" href="#"></a>
+        </article>
+
+
+        <a class="projects__link" href="#">
+            Découvrez tous mes projets
+        </a>
     </section>
     <section class="index__contact">
         <h2 class="contact__title">
             Contactez-moi ! <em>Je ne mord pas...</em>
         </h2>
+        <form class="contact__form">
 
+        </form>
+        <div class="contact__socials">
+
+        </div>
     </section>
+    <div class="index__scroll">
+        <p class="scroll__text">
+            Retourner à la surface ?
+        </p>
+
+    </div>
 </main>
 <?php get_footer(); ?>
